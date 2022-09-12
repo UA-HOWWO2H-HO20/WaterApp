3a909d27407640dd0f3085fcccb2d121b8aafb24
Andy Hansen <arhansen@crimson.ua.edu>
2022-09-12 10:42:16 -0500

    Fix render issues, and add enter key ability

--- water-app/src/search_bar_source/LocationSearchBar.js
+++ water-app/src/search_bar_source/LocationSearchBar.js
@@ -4,6 +4,22 @@ import fetch from 'node-fetch';
 import "./search_bar_style.css"
 
 const LocationSearchBar = (props) => {
+    // Helper to check HTTP error code status
+    const checkResponseCode = (response) => {
+        if(response.ok)
+            return response
+        else
+            // TODO: show error dialogue instead
+            throw new Error('Response to geocode invalid: ${res.status} ${res.statusText}')
+    }
+
+    // Helper to call callback with results
+    const processJSONData = (lat, long) => {
+        console.log("Refreshing page using geocode results (lat: " + lat + ", lng: " + long + ")")
+        props.callback({lng: long, lat: lat, zoom: 7})
+    }
+
+    // Process the input of the text field
     const processInput = () => {
         const data = document.getElementById("location-search-bar-field").value
         console.log("Processing request for location: " + data)
@@ -12,21 +28,19 @@ const LocationSearchBar = (props) => {
         const formattedData = encodeURIComponent(data)
         const apiKey = "d8a222a86303429e9c8ebbac9c9bdb95"
 
-        let fetch = require('node-fetch');
-        let requestOptions = {
-            method: 'GET',
-        };
+        fetch("https://api.geoapify.com/v1/geocode/search?text=" + formattedData + "&apiKey=" + apiKey + "&format=json", { method: 'GET' })
+            .then(checkResponseCode)
+            .then(res => res.json())
+            .then(json => {
+                processJSONData(json.results[0].lat, json.results[0].lon)
+            })
+            .catch(error => console.log("Caught error in geocoding: " + error))
+    }
 
-        const address = 'redacted'
-        const formattedAddress = encodeURIComponent(address)
-
-        const response = fetch("https://api.geoapify.com/v1/geocode/search?text=" + formattedAddress + "&apiKey=" + apiKey, requestOptions).then(response => response.json(true))
-        console.log(response)
-            // .then(response => response.json())
-            // .then(result => console.log(result))
-            // .catch(error => console.log('error', error));
-
-        props.callback({lat: 40.7, long: -73.9, zoom: 7})
+    // Check for enter characters in the box so hitting enter after an address works correctly
+    const checkForEnter = (event) => {
+        if(event.key === 'Enter')
+            processInput()
     }
 
     return (<div id={"location-search-bar"}>
@@ -34,7 +48,7 @@ const LocationSearchBar = (props) => {
             <tbody>
                 <tr>
                     <td id={"location-search-bar-td-first"}>
-                        <input id={"location-search-bar-field"} type="text" placeholder={"Where to?"} />
+                        <input id={"location-search-bar-field"} type="text" placeholder={"Where to?"} onKeyDown={(e) => { checkForEnter(e) }}/>
                     </td>
                     <td id={"location-search-bar-td"}>
                         <button id={"location-search-bar-button"} onClick={() => { processInput() }}>Go!</button>
