6f5bab5b97eff3c9293200cf5cd662512be660cb
Andy Hansen <arhansen@crimson.ua.edu>
2022-10-28 00:16:22 -0500

    Cleanup

--- water-app/src/image_frame_source/ServerRequester.js
+++ water-app/src/image_frame_source/ServerRequester.js
@@ -1,11 +1,24 @@
 import $ from "jquery";
 
+// TODO for server:
+// - Enable Cross-Origin whatever for metadata (CORS missing allow-origin)
+// - Enable the most recent date return for requests
+// - Set data resolution to 1 minute
+// - Add actual data sources
+
+// TODO for frontend
+// - XML parsing of data
+// - better time selection
+// - handling for discrete dates?
+// - Parse and save metadata
+// - Pass better params to loader
+
 // TODO: this class is for testing and needs to be completed later on
 class ServerRequester
 {
     constructor() {
         // Server IP
-        this.hostName = 'http://floviz.undo.it:6789';
+        this.hostName = 'http://floviz.undo.it:6789/geoserver/floviz/wms';
 
         // Limit to the number of frames a user can request so that users don't create ridiculously sized requests
         this.maxImageCount = 250;
@@ -14,16 +27,11 @@ class ServerRequester
         this.imageWidth = 768;
         this.imageHeight = 761;
 
-        // Bounding box
-        this.bbox = '73.884,29.541,76.945,32.575';
+        // Image format
+        this.imageFormat = 'image/jpeg';
 
-        // Other parameters
-        this.srs = 'EPSG%3A4326';
-        this.imageFormat = 'image%2Fjpeg';
-
-        // move this shit andy
-        this.layer = 'floviz:NVDI_data';
-        this.time = '2021-09-14T00:00:00.000Z';
+        // XML parser for metadata
+        this.parser = new DOMParser();
 
         // Bindings
         this.fetchMetaDataFromServer = this.fetchMetaDataFromServer.bind(this);
@@ -37,6 +45,27 @@ class ServerRequester
             // let requestURL = `http://floviz.undo.it:6789/geoserver/floviz/wms?VERSION=1.1.1&REQUEST=GetCapabilities&SERVICE=WMS&`;
             // $.get(requestURL, function(data) {
             //    console.log(data.toString());
+            //
+            //    // Parse data
+            //    const xmlDoc = this.parser.parseFromString(data,"text/xml");
+            //
+            //    // Parse out layers
+            //    const layers = xmlDoc.getElementsByTagName('Layer');
+            //    console.log('Layers: ' + layers.toString());
+            //
+            //    for(const layer of layers) {
+            //        const name = layer.getElementsByTagName('Name')[0];
+            //        const title = layer.getElementsByTagName('Title')[0];
+            //        const srs = layer.getElementsByTagName('SRS')[0];
+            //        const bbox = layer.getElementsByTagName('LatLonBoundingBox')[0];
+            //        const times = layer.getElementsByTagName('Extent')[0];
+            //
+            //        console.log(`Event name: ${name}`);
+            //        console.log(`Title: ${title}`);
+            //        console.log(`SRS: ${srs}`);
+            //        console.log(`BBox: ${bbox}`);
+            //        console.log(`Times: ${times}`);
+            //    }
             // });
 
             // Simulate delay in server response
@@ -63,27 +92,43 @@ class ServerRequester
     // Function that returns URLS to images based on an ID query. The MetaDataSidebar will post events
     // with the id number assigned to a row, and the purpose of this function is to return URLs of images
     // associated with those ids.
-    // TODO: how do we organize this
     getImageURLsFromSelection(ids, imageMetadata, intervalMS, startDate, endDate) {
+        // TODO implement this once everything else works
         // // Calculate number of frames
         // const differenceMS = new Date(endDate).getTime() - new Date(startDate).getTime();
         // let frameCount = Math.floor(differenceMS / intervalMS);
         //
         // // Limit the number of frames returned
-        // if(frameCount > this.maxImageCount)
-        // {
+        // if(frameCount > this.maxImageCount) {
         //     console.log(`User requested ${frameCount} images, which is too many. Downsizing result to ${this.maxImageCount}`);
         //     frameCount = this.maxImageCount;
         // }
-
+        //
+        // let frameDateTimes = [];
+        //
+        // // Build dates of frames
+        // for(let i = 0; i < frameCount; i++) {
+        //     let intervalTime = (i * intervalMS) + new Date(startDate);
+        //     frameDateTimes.push(intervalTime);
+        // }
+        //
+        // // Build list of URLs
         let createdURLs = [];
+        //
+        // for(let i = 0; i < frameCount; i++) {
+        //     const URL = `${this.hostName}?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=${this.imageFormat}&TRANSPARENT=true&STYLES&LAYERS=${this.layer}&exceptions=application%2Fvnd.ogc.se_inimage&SRS=${this.srs}&WIDTH=${this.imageWidth}&HEIGHT=${this.imageHeight}&BBOX=${this.bbox}&time=${frameDateTimes[i]}`;
+        //     createdURLs.push(URL);
+        // }
 
-        let validDates = [[8, 29], [9, 14], [9, 30]];
+        const validDates = [[8, 29], [9, 14], [9, 30]];
+        const layer = 'floviz:NDVI_data';
+        const bbox = '73.3062744140625,28.96820068359375,77.5250244140625,33.14849853515625';
+        const srs = 'EPSG%3A4326';
 
-        for(let i = 0; i < validDates.length; i++)
-        {
+        for(let i = 0; i < validDates.length; i++) {
             let pair = validDates[i];
-            const testURL = `http://floviz.undo.it:6789/geoserver/floviz/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image%2Fjpeg&TRANSPARENT=true&STYLES&LAYERS=floviz%3ANDVI_data&exceptions=application%2Fvnd.ogc.se_inimage&SRS=EPSG%3A4326&WIDTH=768&HEIGHT=761&BBOX=73.3062744140625%2C28.96820068359375%2C77.5250244140625%2C33.14849853515625&time=2021-${pair[0]}-${pair[1]}T00:00:00.000Z`
+            let time = `2021-${pair[0]}-${pair[1]}T00:00:00.000Z`;
+            const testURL = `${this.hostName}?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=${encodeURIComponent(this.imageFormat)}&TRANSPARENT=true&STYLES&LAYERS=${encodeURIComponent(layer)}&exceptions=application%2Fvnd.ogc.se_inimage&SRS=${srs}&WIDTH=${this.imageWidth}&HEIGHT=${this.imageHeight}&BBOX=${encodeURIComponent(bbox)}&time=${time}`;
 
             createdURLs.push(testURL);
         }
