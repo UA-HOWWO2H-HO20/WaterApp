da31ddc2dbe3d5c22e2660c5ea15b36ddb82e56d
Andy Hansen <arhansen@crimson.ua.edu>
2022-11-15 14:51:25 -0600

    Remove sources button

--- water-app/src/footer_source/Footer.js
+++ water-app/src/footer_source/Footer.js
@@ -12,8 +12,6 @@ class Footer extends React.Component {
 
         this.aboutHeader = "About this project"
         this.aboutText = "\tThe water app was created by a team of undergraduate computer science students at the The University of Alabama, as a senior design project. This app was created in collaboration with the Alabama Water Institute, who are responsible for its development and maintenance."
-        this.sourcesHeader = "Data sources"
-        this.sourcesText ="Insert sources here"
     }
 
     render() {
@@ -24,9 +22,6 @@ class Footer extends React.Component {
                         <tr>
                             <td className={"footer-td"}>
                                 <p className={"footer-label"} onClick={() => { this.alertSetStateReference({open: true, headerText: this.aboutHeader, errorText: this.aboutText }) } }>About this project</p>
-                            </td>
-                            <td className={"footer-td"}>
-                                <p className={"footer-label"} onClick={() => { this.alertSetStateReference({open: true, headerText: this.sourcesHeader, errorText: this.sourcesText }) } }>Sources</p>
                             </td>
                             <td className={"footer-td"}>
                                 <a className={"footer-label"} href={"https://github.com/UA-HOWWO2H-HO20/WaterApp"} >View source code</a>
